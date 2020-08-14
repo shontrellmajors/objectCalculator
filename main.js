0ceadec6339e0cf823020965b2e14ca4cb66ffa5
Shontrell Majors <smajors@pivottechschool.com>
2020-08-13 21:17:55 -0500

    Create main.js
    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+let calculator = {
+
+read() {
+  this.a = +prompt("a?", 0);
+  this.b = +prompt("b?", 0);
+},
+
+sum() {
+  return this.a + this.b;
+},
+
+mul() {
+  return this.a * this.b;
+},
+
+div() {
+  return this.a / this.b;
+},
+
+sub() {
+  return this.a - this.b;
+}
+
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
+alert(calculator.div());
+alert(calculator.sub());
